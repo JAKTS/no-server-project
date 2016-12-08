6c428f0c0620dcd1e8af6e9285320eb0911376b6
JAKTS <endodavis@gmail.com>
2016-12-08 14:43:47 -0700

    removed Google API key
    Need to add back to work

--- js/routes-services/apiService.js
+++ js/routes-services/apiService.js
@@ -3,7 +3,7 @@ angular.module('translator').service('apiService', function($http){
   this.getLanguage = function(input, language, target) {
         return $http({
           method: 'GET',
-          url: 'https://www.googleapis.com/language/translate/v2?key=AIzaSyBFOOMPvQmR46jODjEo2yyNqS_JmlJIy5U&source=' + language + '&target=' + target + '&q=' + input
+          url: 'https://www.googleapis.com/language/translate/v2?key=&source=' + language + '&target=' + target + '&q=' + input
 
 
         }).then(function(response){
